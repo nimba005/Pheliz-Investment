b8575ab14368224e23fb9ae8cf7a2506e35d21d2
nimba005 <nimbaoyugi@gmail.com>
2024-05-25 16:16:58 +0300

    main JS

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,36 +1,35 @@
-//changing navigation bar styles on scrolls
-
+//changing navigation bar styles on scroll
 window.addEventListener('scroll', () => {
-  document.quarySelector('nav').classList.toggle
-  ('window-scroll', window.scrollY > 0)
-})
+  document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+});
 
-// show answer
+// show answer in FAQs
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach(faq => {
   faq.addEventListener('click', () => {
     faq.classList.toggle('open');
-//change icon
+
+    //change icon
     const icon = faq.querySelector('.faq__icon i');
     if(icon.className === 'uil uil-plus') {
       icon.className = 'uil uil-minus';
-    }
-    else {
+    } else {
       icon.className = 'uil uil-plus';
     }
-  })
-})
+  });
+});
 
+// Toggle navigation menu
 const menu = document.querySelector('.nav__menu');
 const menuBtn = document.querySelector('#open-menu-btn');
 const closeBtn = document.querySelector('#close-menu-btn');
 
 menuBtn.addEventListener('click', () => {
-  menu.computedStyleMap.display = 'flex';
+  menu.style.display = 'flex';
   closeBtn.style.display = 'inline-block';
   menuBtn.style.display = 'none';
-})
+});
 
 const closeNav = () => {
   menu.style.display = 'none';
